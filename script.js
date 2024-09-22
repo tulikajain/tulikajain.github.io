e2848cc7ccf1f4f476b86e35d436ac809f10e746
Tulika Jain <tulikajain@tulikas-mbp.home>
2024-09-22 15:49:21 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// JavaScript to handle contact form submission
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for your message! We will get back to you soon.');
+});
+
+// window.addEventListener('scroll', function() {
+//     const parallaxElement = document.querySelector('.call-to-action');
+//     let scrolled = window.scrollY;
+
+//     parallaxElement.style.transform = 'translateY(' + scrolled * 0.2 + 'px)';
+// });
